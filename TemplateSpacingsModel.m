8d271fdf8c9e59cbee0593ee10288425a6e59b43
neilberi <nebe@umich.edu>
2023-07-05 14:22:51 -0400

    Added 3pi fitDegree option and error bars on TemplateSpacingModel.m

--- TemplateSpacingsModel.m
+++ TemplateSpacingsModel.m
@@ -8,7 +8,7 @@ close all;
 ParsevalSNR = 1;
 
 %% Read in data
-fitDegree = pi;
+fitDegree = 3*pi;
 
 if (ParsevalSNR == 1)
     filename = sprintf('TemplateSpacingFitCoeffsfitDegree_%fP.csv', fitDegree);
@@ -25,12 +25,12 @@ NParams = length(data(1, :));
 %% Plot Data
 for k = 1:NParams
     figure(k);
-    sk = scatter(log10(abs(fdotvec_sig)), data(:, k), '*k');
+    ek = errorbar(log10(abs(fdotvec_sig)), data(:, k), uncertainties(:, k), '.k');
     titleString = sprintf('Template Spacing Fit Parameter a_{%d} vs fdot', k);
     title(titleString);
     xlabel('Base 10 log of fdot (log(Hz/s))');
     ylabel('Template Spacing Fit Param');
-    sk.LineWidth = 3;
+    ek.LineWidth = 2;
     ax = gca;
     ax.LineWidth = 3;
     ax.FontSize = 16;
@@ -39,7 +39,7 @@ end
 
 %% Fit Data
 
-model_func = @(a, x) a(1).*log10(abs(x)).^(a(2));
+model_func = @(a, x) a(1).*log10(abs(x)) + a(2);
 fitCoeffs = zeros(1, NParams);
 dCoeffs = zeros(1, NParams);
 fitExps = zeros(1, NParams);
@@ -47,7 +47,7 @@ dExps = zeros(1, NParams);
 fdot_sigp = linspace(min(fdotvec_sig), max(fdotvec_sig), 1000);
 
 for k = 1:NParams
-    [fitParamfitParams, dParams] = fitChiSquare(log10(abs(fdotvec_sig)), data(:, k), model_func, -rand(1, 2), zeros(size(fdotvec_sig)), uncertainties(:, k));
+    [fitParamfitParams, dParams] = fitChiSquare(fdotvec_sig, data(:, k), model_func, -rand(1, 2), zeros(size(fdotvec_sig)), uncertainties(:, k));
     %{
     fitCoeffs(k) = fitParamfitParams(1);
     dCoeffs(k) = dParams(1).d;
@@ -57,7 +57,7 @@ for k = 1:NParams
 
     figure(k);
     hold on;
-    pk = plot(log10(abs(fdot_sigp)), model_func(fitParamfitParams, log10(abs(fdot_sigp))), '-r');
+    pk = plot(log10(abs(fdot_sigp)), model_func(fitParamfitParams, fdot_sigp), '-r');
     legend('Calculated Params from const fdot', 'fit', 'Location', 'north');
     hold off;
 end
